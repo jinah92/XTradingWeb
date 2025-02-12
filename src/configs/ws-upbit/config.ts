b2b82eb0789b9e74b9a8a137ac7da43290b327bf
jinah92 <jinah0818@gmail.com>
2025-02-12 13:40:29 +0900

    chore: 사용하지 않는 상수 및 타입 제거

--- src/configs/ws-upbit/config.ts
+++ src/configs/ws-upbit/config.ts
@@ -1,17 +1,1 @@
-import { UpbitTicker } from '../../types/ticker';
-
 export const UPBIT_URL = import.meta.env.VITE_WS_UPBIT_URL;
-export const UPBIT_TICKER_MESSAGE = [
-  { ticket: 'test' },
-  { type: 'ticker', codes: ['KRW-BTC', 'KRW-ETH', 'KRW-ETC', 'KRW-NEO', 'KRW-MTL', 'KRW-XRP', 'KRW-SNT', 'KRW-WAVES'] },
-];
-export const COIN_LIST: UpbitTicker[] = [
-  { code: 'KRW-BTC' },
-  { code: 'KRW-ETH' },
-  { code: 'KRW-ETC' },
-  { code: 'KRW-NEO' },
-  { code: 'KRW-MTL' },
-  { code: 'KRW-XRP' },
-  { code: 'KRW-SNT' },
-  { code: 'KRW-WAVES' },
-];
