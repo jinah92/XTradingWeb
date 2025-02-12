c7e36f6831e880892602b9c2b489f5efcd5dad4e
jinah92 <jinah0818@gmail.com>
2025-02-12 15:40:40 +0900

    feat: 마켓 Tab 및 Tab별 콘텐츠 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,31 @@
-import React, { useMemo, useRef, useState } from 'react';
-import { UseWebSocket } from '../hooks/websocket/use-websocket';
-import { AgGridReact } from 'ag-grid-react';
-import { GridApi, GridReadyEvent } from 'ag-grid-community';
-
-import { UPBIT_URL } from '../configs/ws-upbit/config';
+import React from 'react';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
-import { useSelectMarketsQuery } from '../queries/upbit';
-import { MarketType, UpbitTicker } from '../apis/market';
-import { realtimeCurrencyOptions } from '../configs/chart/upbit-ticker';
+import { MarketType } from '../apis/market';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
+import { MarketsOverviewChart } from '../components/chart/markets/marketsOverviewChart';
 
 const Home = () => {
-  const gridApiRef = useRef<GridApi<UpbitTicker> | null>(null);
-  const [marketType] = useState<MarketType>('KRW');
-  const { data } = useSelectMarketsQuery(marketType);
-
-  const tickerMessage = useMemo(() => {
-    if (data) {
-      return [
-        {
-          ticket: 'currency_status',
-        },
-        { type: 'ticker', codes: data?.map(item => item.market) },
-      ];
-    }
-    return undefined;
-  }, [data]);
-
-  const { isConnect } = UseWebSocket(UPBIT_URL, tickerMessage, onMessage);
-
-  async function onMessage(event: MessageEvent) {
-    const text = await (event.data as Blob).text();
-    const json = JSON.parse(text) as UpbitTicker;
-    if (gridApiRef.current) {
-      const rowNode = gridApiRef.current?.getRowNode(json.code as string);
-      rowNode?.updateData(json);
-    }
-  }
-
-  const onGridReady = (params: GridReadyEvent<UpbitTicker, any>) => {
-    gridApiRef.current = params.api;
-  };
-
   return (
-    <div className="p-3 w-full h-">
-      {isConnect && (
-        <div className="ag-theme-alpine h-[70vh] overflow-auto">
-          <AgGridReact<UpbitTicker> onGridReady={onGridReady} rowData={data} {...realtimeCurrencyOptions} />
-        </div>
-      )}
+    <div className="p-3 w-full">
+      <div className="ag-theme-alpine overflow-auto">
+        <Tabs className="w-full" defaultValue="ALL">
+          <TabsList className="grid w-full grid-cols-4">
+            {Array.from<MarketType>(['ALL', 'KRW', 'BTC', 'USDT']).map(market => (
+              <TabsTrigger key={market} value={market}>
+                {market}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {Array.from<MarketType>(['ALL', 'KRW', 'BTC', 'USDT']).map(market => (
+            <TabsContent className="h-[800px]" key={market} value={market}>
+              <MarketsOverviewChart type={market} />
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </div>
   );
 };
