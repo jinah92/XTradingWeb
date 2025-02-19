bab700546cf713c33f81aefb3d8efeb518f32311
jinah92 <jinah0818@gmail.com>
2025-02-19 09:29:06 +0900

    feat: market 관련 타입 수정, ticker 타입 추가

--- src/apis/market/model.ts
+++ src/apis/market/model.ts
@@ -1,55 +1,77 @@
 import { Time } from 'lightweight-charts';
 
-type PriceChange = 'RISE' | 'EVEN' | 'FALL';
+export type PriceChange = 'RISE' | 'EVEN' | 'FALL';
 
 export type MarketType = 'KRW' | 'BTC' | 'USDT' | 'ALL';
 
-interface MarketEvent {
-  warning: boolean;
-  caution: object;
-}
-
-export interface UpbitTicker {
-  code?: string;
-  korean_name?: string;
-  english_name?: string;
-  trade_price?: number;
-  prev_closing_price?: number;
-  change?: PriceChange;
-  change_rate?: number;
-  signed_change_rate?: number;
-  acc_trade_volume_24h?: number;
-  acc_trade_price_24h?: number;
-}
-
-export interface UpbitTicker {
+export interface RawUpbitMarketData {
   market: string;
-  market_event: MarketEvent;
-}
-
-export interface UpbitMarketCandleResponse {
-  market: string;
-  candle_date_time_utc: string;
-  candle_date_time_kst: string;
+  trade_date: string;
+  trade_time: string;
+  trade_date_kst: string;
+  trade_time_kst: string;
+  trade_timestamp: number;
   opening_price: number;
   high_price: number;
   low_price: number;
   trade_price: number;
+  prev_closing_price: number; // utc-time 0:00 기준
+  change: PriceChange;
+  change_price: number;
+  change_rate: number;
+  signed_change_price: number;
+  signed_change_rate: number;
+  trade_volume: number;
+  acc_trade_price: number; // utc-time 0:00 기준
+  acc_trade_price_24h: number;
+  acc_trade_volume: number; // utc-time 0:00 기준
+  acc_trade_volume_24h: number;
+  highest_52_week_price: number;
+  highest_52_week_date: number;
+  lowest_52_week_price: number;
+  lowest_52_week_date: string;
   timestamp: number;
+}
+
+interface UbitMarketCandle {
+  candle_date_time_utc: string;
+  candle_date_time_kst: string;
   candle_acc_trade_price: number;
   candle_acc_trade_volume: number;
 }
 
+export interface UpbitMarketCandleResponse
+  extends Pick<
+      RawUpbitMarketData,
+      'market' | 'opening_price' | 'high_price' | 'low_price' | 'trade_price' | 'timestamp'
+    >,
+    UbitMarketCandle {}
+
 // day
-export interface UpbitMarketShortTermCandleResponse extends UpbitMarketCandleResponse {
-  prev_closing_price: number;
-  change_price: number;
-  change_rate: number;
+export interface UpbitMarketShortTermCandleResponse
+  extends Pick<
+      RawUpbitMarketData,
+      | 'market'
+      | 'opening_price'
+      | 'high_price'
+      | 'low_price'
+      | 'trade_price'
+      | 'timestamp'
+      | 'prev_closing_price'
+      | 'change_price'
+      | 'change_rate'
+    >,
+    UbitMarketCandle {
   converted_trade_price: number;
 }
 
 // week, month, year
-export interface UpbitMarketLongTermCandleResponse extends UpbitMarketCandleResponse {
+export interface UpbitMarketLongTermCandleResponse
+  extends Pick<
+      RawUpbitMarketData,
+      'market' | 'opening_price' | 'high_price' | 'low_price' | 'trade_price' | 'timestamp'
+    >,
+    UbitMarketCandle {
   first_day_of_period: string;
 }
 
