c5756bf009f45a2fda519399188a159b26858c4f
jinah92 <jinah0818@gmail.com>
2025-02-21 09:35:52 +0900

    chore: eslint, vite 설정파일 업데이트 - eslint json포맷 변경 - tsconfigPath 플러그인 설치 및 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
-import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-        '@shared': path.resolve(__dirname, './src/shared'),
-      },
-    },
+    plugins: [react(), tsconfigPaths()],
     build: {
       sourcemap: true,
     },
