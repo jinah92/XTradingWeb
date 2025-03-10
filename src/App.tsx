58fc398bc4d115b6aee155bd772a534c8747265d
jinah92 <jinah0818@gmail.com>
2025-03-11 08:51:13 +0900

    feat: route 내부 auth provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,23 @@ import { CookiesProvider } from 'react-cookie';
 
 import { Toaster } from '@shared';
 
+import { AuthProvider } from '@/router/AuthContext';
 import CommonRouter from '@/router/CommonRouter';
 
-import { AuthProvider } from './router/AuthContext';
 import { ThemeProvider } from './ThemeProvider';
 
 import './App.css';
 
 function App() {
   return (
-    <>
-      <AuthProvider>
-        <CookiesProvider>
-          <ThemeProvider>
-            <Toaster />
-            <CommonRouter />
-          </ThemeProvider>
-        </CookiesProvider>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <CookiesProvider>
+        <ThemeProvider>
+          <Toaster />
+          <CommonRouter />
+        </ThemeProvider>
+      </CookiesProvider>
+    </AuthProvider>
   );
 }
 
--- src/router/CommonRouter.tsx
+++ src/router/CommonRouter.tsx
@@ -14,40 +14,37 @@ import ChartView from '@/pages/market/ChartView';
 import Market from '@/pages/market/Market';
 import MyPage from '@/pages/mypage/MyPage';
 import News from '@/pages/news/News';
-import { AuthProvider } from '@/router/AuthContext';
 import PrivateRoute from '@/router/PrivateRoute';
 
 import MarketDetail from '../pages/market/MarketDetail';
 
 function CommonRouter() {
   return (
-    <AuthProvider>
-      <Routes>
-        {/* // <Route>의 element에 레이아웃을 넣어준다. */}
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/interest" element={<Interest />} />
-          <Route path="/market" element={<Market />} />
-          <Route path="/market/:marketId" element={<MarketDetail />} />
-          <Route path="/chart" element={<ChartView />} />
-          <Route element={<BoardLayout />}>
-            <Route path="/board/idea" element={<IdeaListPage />} />
-            <Route path="/board/feed" element={<FeedListPage />} />
-          </Route>
+    <Routes>
+      {/* // <Route>의 element에 레이아웃을 넣어준다. */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/interest" element={<Interest />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/market/:marketId" element={<MarketDetail />} />
+        <Route path="/chart" element={<ChartView />} />
+        <Route element={<BoardLayout />}>
+          <Route path="/board/idea" element={<IdeaListPage />} />
+          <Route path="/board/feed" element={<FeedListPage />} />
         </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/mypage" element={<MyPage />} />
-        </Route>
-        <Route element={<NewsLayout />}>
-          <Route path="/news" element={<News />} />
-          <Route path="/news/:id" element={<News />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
-        {/*// TODO: 404 처리 필요*/}
-        {/*<Route component={NotFound} />*/}
-      </Routes>
-    </AuthProvider>
+      </Route>
+      <Route element={<PrivateRoute />}>
+        <Route path="/mypage" element={<MyPage />} />
+      </Route>
+      <Route element={<NewsLayout />}>
+        <Route path="/news" element={<News />} />
+        <Route path="/news/:id" element={<News />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
+      {/*// TODO: 404 처리 필요*/}
+      {/*<Route component={NotFound} />*/}
+    </Routes>
   );
 }
 
